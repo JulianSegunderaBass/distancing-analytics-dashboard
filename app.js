497d3aca2da74af7ab2915407038f3d8a4d7b483
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-09-05 22:39:50 +0800

    Initial logic for saving and deleting records

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 // Models
 const CurrentData = require('./models/currentData');
 const OldData = require('./models/oldData');
@@ -26,6 +27,8 @@ app.set('views', path.join(__dirname, 'views'));
 // Accessing static assets
 app.use(express.static(path.join(__dirname, 'javascripts')));
 app.use(express.urlencoded({ extended: true }));
+// For other methods
+app.use(methodOverride('_method'));
 
 // Routes
 app.get('/', async (req, res) => {
@@ -34,6 +37,39 @@ app.get('/', async (req, res) => {
     const currentData = data[0];
     const oldData = await OldData.find({});
     res.render('index', { currentData, oldData });
+});
+
+app.post('/', async (req, res) => {
+    const date = new Date();
+    const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+    const data = await CurrentData.find({});
+    // Getting first element of currentData array
+    const currentData = data[0];
+
+    // Saving current data as archived record
+    const archivedData = new OldData({
+        recordDate: `${month}-${day}-${year}`,
+        violationCount: currentData.violationCount,
+        headcount: currentData.headcount
+    });
+    await archivedData.save();
+
+    // Deleting the currentData record
+    const deleteRecord = await CurrentData.deleteMany({});
+
+    // Creating new random current data
+    const newData = new CurrentData({
+        violationCount: Math.floor(Math.random() * 500),
+        headcount: Math.floor(Math.random() * 1000)
+    });
+    await newData.save();
+
+    res.redirect('/');
+});
+
+app.delete('/:recordID', async (req, res) => {
+    const record = await OldData.findByIdAndDelete(req.params.recordID);
+    res.redirect('/');
 });
 
 // Setting port
